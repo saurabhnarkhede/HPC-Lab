f755d4510d567a8651833e0db28a28c14cae5a8f
saurabhnarkhede <48806080+saurabhnarkhede@users.noreply.github.com>
2021-11-22 12:21:44 +0530

    Add files via upload

---
+++ Assignment08/matmul_unopt.cu
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<cuda.h>
+#define row1 4 /* Number of rows of first matrix */
+#define col1 5 /* Number of columns of first matrix */
+#define row2 5 /* Number of rows of second matrix */
+#define col2 4 /* Number of columns of second matrix */
+
+__global__ void matproduct(int *l,int *m, int *n)
+{
+    int x=blockIdx.x;
+    int y=blockIdx.y;
+    int k;
+  
+n[col2*y+x]=0;
+for(k=0;k<col1;k++)
+   {
+    n[col2*y+x]=n[col2*y+x]+l[col1*y+k]*m[col2*k+x];
+   }
+}
+
+int main()
+{
+    int a[row1][col1] = {{1,2,3,4,5}, {4,5,6,7,8},{6,7,8,9,1},{2,3,5,4,5}};
+    int b[row2][col2] = {{1,2,3,4}, {4,5,2,3}, {8,9,10,11},{4,3,5,2},{5,4,7,8}};
+    int c[row1][col2];
+    int *d,*e,*f;
+    int i,j;
+
+
+    cudaMalloc((void **)&d,row1*col1*sizeof(int));
+    cudaMalloc((void **)&e,row2*col2*sizeof(int));
+    cudaMalloc((void **)&f,row1*col2*sizeof(int));
+
+ cudaMemcpy(d,a,row1*col1*sizeof(int),cudaMemcpyHostToDevice);
+ cudaMemcpy(e,b,row2*col2*sizeof(int),cudaMemcpyHostToDevice);
+
+dim3 grid(col2,row1);
+/* Here we are defining two dimensional Grid(collection of blocks) structure. Syntax is dim3 grid(no. of columns,no. of rows) */
+
+    matproduct<<<grid,1>>>(d,e,f);
+
+ cudaMemcpy(c,f,row1*col2*sizeof(int),cudaMemcpyDeviceToHost);
+    printf("\nProduct of two matrices:\n ");
+    for(i=0;i<row1;i++)
+    {
+        for(j=0;j<col2;j++)
+        {
+              printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+
+    cudaFree(d);
+    cudaFree(e);
+    cudaFree(f);
+
+    return 0;
+}
